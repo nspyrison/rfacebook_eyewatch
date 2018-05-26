ae4845888f42611fbee21f566b188ce62a234ae1
Nicholas Spyrison <nicholas.spyrison@monash.edu>
2018-05-26 16:09:46 +1000

    going to package/vignette form. running into package issues. data isn't being returned by pull fb posts. check in  to work on spinifex

---
+++ R/pull_fb_posts.r
@@ -0,0 +1,23 @@
+pull_fb_posts <- function(pages) {
+  pages <- gsub(" ", "", pages, fixed = TRUE) %>% tolower()
+  n_pages <- length(unique(pages))
+  
+  posts_raw <- NULL
+  start_time <- Sys.time()
+  for(i in 1:n_pages){
+    delta <- getPage(pages[i], token, n=200, feed = TRUE) %>% 
+      as_tibble() %>% cbind("page" = pages[i])
+    posts_raw <- rbind(posts_raw, delta)
+  } 
+  end_time <- Sys.time()
+  pull_time <- end_time - start_time 
+  pull_rows <- nrow(posts_raw)
+  file <- paste0("data/eyewatch_posts_raw_", Sys.Date(),".rda")
+  save(posts_raw, file = file)
+  
+  print(pull_rows, " posts pulled from ", n_pages, 
+        " pages. Data pulled in ", pull_time)
+  print("Data has been saved to ", file, ". The path is also returned by this function.")
+  
+  return(file())
+}
--- _eyewatch_main.r
+++ _eyewatch_main.r
@@ -1,22 +1,54 @@
 # Facebook Eyewatch data
 # Nicholas Spyrison 
 # May 2018
-library(tidyverse) 
-library(plotly)
+library(dplyr)
 library(Rfacebook)
+#library(tidyverse)
+#library(plotly)
+#library(tibble)
 
-token <- "EAACEdEose0cBABDfswl3dCe1uUCrFgfve6gJ6cKtBMcfUxVzSkLuL01SaXE3FwIbZCZC7tv6ftD6pXLIzcaSeMW3FGzpVlL3orUlbw3vGeJ56cDMFrTgXxZBEQxbGvhle7X1R0KSkPKnD2x9zXYv0AYfCwcY3ctHu8IzaLrKLXSFjUKSZAKnXAg1hZAuMgjMZD" 
+source(".\\R\\pull_fb_posts.r")
+
+token <- "EAACEdEose0cBAEM4hwfLCrIZBI1pHhtGOIaw1ejkgdhKzgG8bGVcqLNA75Jvl73AKNTQW6OVV3X56bp86GHM6A5uOkskXnZC4kmo1kfkYwavM22SvhtbgNaC81ZBsF65aSSgjDjbKCWX1sMHZAQu8JlmuLejkWz88ei5NmNC7FxXUgyivF4V0Iiacc0SCOUZD" 
 #temp token, will need to gen each time.
-#https://developers.facebook.com/tools/explorer/?method=GET&path=me%3Ffields%3Did%2Cname&version=v3.0)
+#browseURL("https://developers.facebook.com/tools/explorer/?method=GET&path=me%3Ffields%3Did%2Cname&version=v3.0)")
 
-eyewatch_posts_raw <- NULL
-start_time <- Sys.time()
-for(i in 1:length(station)){
-  delta <- getPage(station[i], token, n=200, feed = TRUE) %>% as_tibble() %>% cbind("station" = station[i])
-  eyewatch_posts_raw <- rbind(eyewatch_posts_raw, delta)
-} 
-end_time <- Sys.time()
-end_time - start_time #1.04 min, 952 x 11. ballarat, 5:30pm 16 May 2018
+#station <- cat("eyewatch",c("Wyndham", "Melton", "Whittlesea", "Cardinia", "Latrobe", "Ballarat", "Brimbank", "Greater Shepparton", "Greater Dandenong", "Frankston", "Knox", "Geelong"))
+
+# not in email: "Monash", "Hume", "Kingston", "Latrobe", "Boroondara", "Yarra Ranges", "Bass Coast", "Warrnambool", "Casey", "Mildura", "Swan Hill", "Mornington Peninsula", "Darebin", "Moreland", "Bendigo", "Horsham", "Moorabool", "Baw Baw", "Hobsons Bay", "Benalla", "Northern Grampians"  __ anadditional 21 stations not mentioned. 33 in total
+
+s <- "eyewatchMonash, eyewatchMelton"
+pages <- s
+pull_fb_posts(s)
+
+#file <- paste0("data/eyewatch_posts_raw_", Sys.Date(),".rda")
+load(file)
+
+
+
+pull_fb_posts <- function(pages) {
+  pages <- gsub(" ", "", pages, fixed = TRUE) %>% tolower()
+  n_pages <- length(unique(pages))
+  
+  posts_raw <- NULL
+  start_time <- Sys.time()
+  for(i in 1:n_pages){
+    delta <- getPage(pages[i], token, n=25, feed = TRUE) %>% 
+      as_tibble() %>% cbind("page" = pages[i])
+    posts_raw <- rbind(posts_raw, delta)
+  } 
+  end_time <- Sys.time()
+  pull_time <- end_time - start_time 
+  pull_rows <- nrow(posts_raw)
+  file <- paste0("data/eyewatch_posts_raw_", Sys.Date(),".rda")
+  save(posts_raw, file = file)
+  
+  print(pull_rows, " posts pulled from ", n_pages, 
+        " pages. Data pulled in ", pull_time)
+  print("Data has been saved to ", file, ". The path is also returned by this function.")
+
+  return(file())
+}
 
 #save(eyewatch_posts_raw, file = "data/eyewatch_posts_raw.rda")
 

7f6c172beaf057a0ad20e8ff3d65439f7b089a65
Nicholas Spyrison <nicholas.spyrison@monash.edu>
2018-06-09 20:51:56 +1000

    pull fb reactions. think of better output name?

--- R/pull_fb_reactions.r
+++ R/pull_fb_reactions.r
@@ -1,29 +1,70 @@
+#file= "data/posts_raw_2018_06_03.rda"
+#token <- "EAACEdEose0cBALYAsYI2VO5aPTKOeyd3bZAl2JR0k8T1sSUM40pb2bLuqDngVhZCuTRmUVhC7hVDO1haRZCumcXnvl39rMQA1hScRaDvZCLH2lY0GaTtZAxp0my2LWrELlQfbFpjKytz2226WWDej9ZCo2VZBwma2oeKgIuIIvvNVd0ZBlz1nP2XyxYZArN2ToFYIZC7kTbH512gZDZD" 
 pull_fb_reactions <- function(file, token) {
+  tic <- Sys.time()
   stopifnot(is.character(file))
   stopifnot(file.exists(file))
+  stopifnot(grepl("posts_raw_", file)) # expects raw posts file.
   require(Rfacebook)
+  require(dplyr)
   
-  ### PULL REACTIONS
-  #might need to make this go in order, and use exact object name, rather than dynamic.
+  ### ETL
   load(file = file)
-  filename = substr(file, 6, nchar(file) - 4) #SPECIFIC TO 'data/' % '.rda'
+  filename <- substr(file, 6, nchar(file) - 4) #SPECIFIC TO 'data/' % '.rda'
   
-  posts_obj <- eval(parse(text = filename))
-  posts_obj <- as.data.frame(posts_obj)
+  posts_raw <- as.data.frame(posts_raw)
+  n_posts_in <- nrow(posts_raw)
   
-  #WORK HERE !!!!!!
-  getReactions(post = posts_obj$id[i], token=fb_oauth)
+  ### getPost()
+  n_posts <- nrow(posts_raw)
+  comments_raw <- NULL
+  postinfo_raw <- NULL
+  for (i in 1:n_posts) {
+    ith_post <- getPost(post = posts_raw$id[i], token = token)
+    #postinfo_raw <- rbind(postinfo_raw, ith_post[[1]]) 
+      #dup of the post info from getPage()
+    comments_raw <- rbind(comments_raw, ith_post[[2]])
+  } #error on 10 out of 20. HOW TO TRYCATCH ERRORS?
+  comments_raw <- as.data.fram(comments_raw)
   
+  ### getReactions()
+  reactions_raw <- NULL
+  for (i in 1:n_posts) {
+    ith_reaction <- getReactions(post = posts_raw$id[i], token = token)
+    reactions_raw <- rbind(reactions_raw, ith_reaction)
+  } #error on 10 out of 20. HOW TO TRYCATCH ERRORS?
   
+  posts_raw <- as.data.frame(left_join(posts_raw, reactions_raw, by = "id",
+                         suffix = c(".x", ".y") ) )
   
   ### RETURN
-  filename <- paste0("posts_clean_", gsub("-", "_", Sys.Date()))
+  stopifnot(ncol(comments_raw) == 7)
+  n_comments <- nrow(comments_raw)
+  n_posts_out <- nrow(posts_raw)
+  cfilename <- gsub("posts", "comments", filename)
+  cfile <- paste0("data/", cfilename,".rda")
+  eval(parse(text = paste0(cfilename, " <- comments_raw")))
+  save(list = cfilename, file = cfile)
+  
+  print(paste0(n_comments, "comments pulled from ", n_posts_out, " posts."))
+  print(paste0("Raw comment data has been saved to  ", file, "."))
+  
+  n_posts_out <- nrow(posts_raw)
+  stopifnot(n_posts_in == n_posts_out)
+  stopifnot(ncol(posts_raw) == 18)
+  
+  filename <- gsub("posts", "postsreactions", filename)
   file <- paste0("data/", filename,".rda")
-  eval(parse(text = paste0(filename, " <- posts_clean")))
+  eval(parse(text = paste0(filename, " <- posts_raw")))
   save(list = filename, file = file)
   
-  print(paste0("Clean data has been saved to  ", file, ".  The filepath is also returned by this function."))
-  stopifnot(ncol(posts_clean) == 18)
+  toc <- Sys.time()
+  runtime <- toc - tic
+  print(paste0("Function ran in:") )
+  print(runtime)
+  print(paste0("\nRaw post and reaction data has been saved to  ", file, 
+                ". The filepath is also returned by this function."))
+  
   return(file)
 }
 

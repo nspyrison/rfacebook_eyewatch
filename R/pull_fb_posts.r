d599f2a87a3bf6022322f0ab8887b76a7dd390ad
Nicholas Spyrison <nicholas.spyrison@monash.edu>
2018-06-09 20:52:50 +1000

    pull fb posts. cleanup

--- R/pull_fb_posts.r
+++ R/pull_fb_posts.r
@@ -1,4 +1,8 @@
+#pages <- "eyewatchBallarat"
+#n_ppp = 42
+#token <- "EAACEdEose0cBALYAsYI2VO5aPTKOeyd3bZAl2JR0k8T1sSUM40pb2bLuqDngVhZCuTRmUVhC7hVDO1haRZCumcXnvl39rMQA1hScRaDvZCLH2lY0GaTtZAxp0my2LWrELlQfbFpjKytz2226WWDej9ZCo2VZBwma2oeKgIuIIvvNVd0ZBlz1nP2XyxYZArN2ToFYIZC7kTbH512gZDZD" 
 pull_fb_posts <- function(pages, n_ppp = 1000, token) {
+  tic <- Sys.time()
   stopifnot(is.character(pages))
   require(Rfacebook)
   
@@ -9,29 +13,32 @@ pull_fb_posts <- function(pages, n_ppp = 1000, token) {
   n_expected_posts <- n_pages * n_ppp
   
   posts_raw <- NULL
-  start_time <- Sys.time()
   for(i in 1:n_pages){
     delta <- getPage(pages[i], token, n = n_ppp, feed = TRUE)
     delta <- cbind(delta, "page" = pages[i])
     posts_raw <- rbind(posts_raw, delta)
   } 
-  end_time <- Sys.time()
   posts_raw <- as.data.frame(posts_raw)
   
   ### RETURN
+  stopifnot(ncol(posts_raw) == 12)
   filename <- paste0("posts_raw_", gsub("-", "_", Sys.Date()))
   file <- paste0("data/", filename,".rda")
   eval(parse(text = paste0(filename, " <- posts_raw")))
   save(list = filename, file = file)
   
   if (nrow(posts_raw) != n_expected_posts) 
-    message(paste0("FYI: Not all pages had ", n_ppp, " posts."))
-  pull_time <- end_time - start_time 
+    message(paste0("FYI: Not all pages had ", n_ppp, " posts.") )
+            
+
   pull_rows <- nrow(posts_raw)
-  message(paste0("\n", pull_rows, " posts pulled from ", n_pages, 
-        " page(s). Data pulled in ", round(pull_time, 2), "seconds"))
-  message(paste0("\nRaw data has been saved to  ", file, "  , the filepath is also returned by this function."))
+  toc <- Sys.time()
+  runtime <- toc - tic 
+  print(paste0(pull_rows, " posts pulled from ", n_pages, " page(s).") ) 
+  print(paste0("Function ran in:") )
+  print(runtime)
+  print(paste0("Raw data has been saved to  ", file, "  , the filepath 
+                 is also returned by this function.") )
   
-  stopifnot(ncol(posts_raw) == 12)
   return(file)
 }

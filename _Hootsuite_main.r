e54a519ca95eb4549abe5edc0e14c3836e37cac9
Nicholas Spyrison <spyrison@gmail.com>
2018-09-20 15:58:51 +1000

    Hootsuite feasibility. checkin before data capture.

--- _Hootsuite_main.r
+++ _Hootsuite_main.r
@@ -1,12 +1,22 @@
 # Hootsuite feasibility
 # NS 16-09-2018
 
+library(tidyverse)
+library(lubridate)
 
-?read.csv
-dat <- read.csv(header = TRUE,
-         file = "./data/facebook_post_performance_2018-09-06_to_2018-09-12_created_on_20180912T0550Z_facebook_post_performance.csv"
+#C:\Users\spyri\Documents\R\rfacebook_eyewatch\data\facebook_postreactions_ns_2018-07-01_to_2018-09-19_created_on_20180920T0140Z
+?read_csv
+dat <- read_csv("./data/facebook_post_performance_2018-09-06_to_2018-09-12_created_on_20180912T0550Z_facebook_post_performance.csv"
+)
+
+### Jul1-Sept19:
+dat <- read_csv("./data/facebook_postreactions_ns_2018-07-01_to_2018-09-19_created_on_20180920T0140Z/facebook_postreactions_ns_2018-07-01_to_2018-09-19_created_on_20180920T0140Z_facebook_posts.csv"
 )
 View(dat)
+
+dat <- as.tibble(dat)
+summary(dat$`Date (GMT)`)
+
 # Removed: Tags, Campaign, Organic.Impressions, Organic.Reach, Other.Actions,
 ## Photo.Views, Reactions..Other
 

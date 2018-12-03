ed8545873027a58e7b1c0a06489d3f5d6a1c180a
Nicholas Spyrison <nicholas.spyrison@monash.edu>
2018-12-03 16:58:03 +1100

    Clean,filt function done. cleaning

--- 2_R/clean_hs_fb_posts.r
+++ 2_R/clean_hs_fb_posts.r
@@ -5,27 +5,32 @@ library(lubridate)
 library(tsibble)
 library(dplry)
 
+
 ## EXAMPLE:
-path <- "./1_raw_data/facebook_postreactions_ns_2017-11-18_to_2018-11-29_created_on_20181130T0509Z_facebook_posts.csv"
-clean_hs_fb_posts(path)
+#path <- "./1_raw_data/facebook_postreactions_ns_2017-11-18_to_2018-11-29_created_on_20181130T0509Z_facebook_posts.csv"
+path <- "./1_raw_data/facebook_postreactions_ns_2017-11-18_to_2018-09-19_created_on_20180920T0920Z_facebook_posts.csv"
+c_rdapath <- clean_hs_fb_posts(path)
   ## for all 55 stations.
 
 clean_hs_fb_posts <- function(path) {
   ### ASSERT AND LOAD
   stopifnot(is.character(path))
+  stopifnot(length(path) == 1)
   stopifnot(file.exists(path))
+  
   filename = substr(path, 14, nchar(path) - 4) 
     #SPECIFIC TO './1_raw_data/' & '.csv'
   
-  dat_in <- tibble::as_tibble(readr::read_csv(path))
+  dat_in <- suppressMessages(tibble::as_tibble(readr::read_csv(path)))
   dat <- dat_in
   
-  if (nrow(dat) == 10000) warning("Data contains exactly 10,000 rows, make sure it contains the expected date range")
+  if (nrow(dat) == 10000) warning("Data contains exactly 10,000 rows, Data export may have been turncated by Hootsuite")
   
   ### STAGE AND CLEAN
   dat <- dplyr::rename(.data = dat, 
                        `Datetime`      = `Date (GMT)`,
-                       `PagePostID`    = `Facebook Post ID`,
+                       `PagePostID`   = `Post ID`, # Old col name.
+                       #`PagePostID`    = `Facebook Post ID`, # New col name.
                        `PostLink`      = `Post Permalink`,
                        `PostType`      = `Post Type`,
                        `PostMessage`   = `Post Message`,
@@ -36,7 +41,10 @@ clean_hs_fb_posts <- function(path) {
   
   dat <- dplyr::mutate(
     .data = dat,
-    `Station`            = as.factor(substr(`Facebook Page`, 12, 100)),
+    `Station`            = as.factor(
+      gsub(" Police Service Area", "", gsub("Eyewatch - ", "" , gsub(
+        "â€“", "-", `Facebook Page`)))# Trim page names to LGA name.
+    ),
     `Date`               = as.Date(`Datetime`),
     `Time`               = substr(`Datetime`, 12, 23),
     `Hour`               = as.factor(substr(`Datetime`, 12, 13)),
@@ -54,37 +62,69 @@ clean_hs_fb_posts <- function(path) {
     `CommentCount`       = as.integer(`CommentCount`),
     `ShareCount`         = as.integer(`ShareCount`),
     `EngagementCount`    = `ReactionCount` + `CommentCount` + `ShareCount`,
-    `PostMessage`        = 
-      ifelse(`PostMessage` == "(Post with no description)", NA, `PostMessage`),
+    `PostMessage`        = ifelse(`PostMessage` == "(Post with no description)", NA, `PostMessage`),
+    `DateAquired`        = as.Date(substr(path, nchar(path) - 32, nchar(path) - 25), "%Y%m%d"),
     `IsCsnStation`       = ifelse (`Station` %in% c(
       "Wyndham", "Melton", "Whittlesea", "Cardinia", "Latrobe", "Ballarat", 
       "Brimbank", "Greater Shepparton", "Greater Dandenong", "Frankston", 
       "Knox", "Geelong"), "CSN", "not CSN")
     #`HasVicpolLink`      = (grepl("vicpol", `PostLink`) & `PostType` == "link"),
     #`HasHttpsVicpolLink` = (grepl("https", `PostLink`) & `HasVicpolLink` == T)
+      # Only the ballarat sample data had direct vic pol links. 
+      # Now only the Victoria Police Page has fb/victoriapolice links.
   )
   
   clean_dat <- dat[
     ,c("PagePostID", "Station",  "Datetime", "PostLink", "PostType",
        "PostMessage", "ReactionCount", "CommentCount", "ShareCount",
-       "EngagementCount", "IsCsnStation", "Date",
+       "EngagementCount", "DateAquired", "IsCsnStation", 
        #"HasVicpolLink", "HasHttpsVicpolLink"
-       "Time", "Hour", "Year", "Quarter", "Month", "Week", "YearQuarter",
-       "YearMonth", "YearWeek", "Weekday", "PostDaysOld")
+       "Date", "Time", "Hour", "Year", "Quarter", "Month", "Week", 
+       "YearQuarter", "YearMonth", "YearWeek", "Weekday", "PostDaysOld")
     ]
+  
+  ### FILTER
+  filt_dat <- clean_dat
+  
+  filt_dat <- dplyr::filter(filt_dat, PostType != "music")
+  
+  ### IF MORE THAN 3 DAYS ARE MISSING FROM THE START OR END MONTH,
+  ###   ASSUME THE MONTH IS INCOMPLETE AND DROP IT.
+  startDate <- min(filt_dat$`Date`)
+  endDate   <- max(filt_dat$`Date`)
+  startDay  <- as.integer(substr(startDate,9,10))
+  endDay    <- as.integer(substr(endDate,9,10))
+  startDaysAfterFirst <- startDay - 1
+  endDaysBeforeLast <- as.integer(lubridate::days_in_month(endDate) - endDay)
+  if (startDaysAfterFirst > 3) {
+    daysTillEoM <- as.integer(lubridate::days_in_month(startDate) - startDay)
+    dateFloor <- lubridate::as_date(startDate + daysTillEoM)
+    filt_dat <- dplyr::filter(filt_dat, `Date` > dateFloor)
+  }
+  if (endDaysBeforeLast > 3) {
+    dateCeiling <- lubridate::as_date(endDate - endDay + 1)
+    filt_dat <- dplyr::filter(filt_dat, `Date` < dateCeiling)
+  }
+  
+  ### Order PostType by ~asc mean EngagementCount (hard coded prior)
+  filt_dat$PostType = factor(
+    filt_dat$PostType, levels = c("Event", "Video", "Link", "Status", "Photo"))
+  output <- filt_dat
   
   ### RETURN
   filename <- paste0(filename, "_cleaned")
-  path <- paste0("./3_staged_data/", filename, ".rda")
-  eval(parse(text = paste0(filename, " <- clean_dat")))
-    # assign clean_dat to filename
-  save(list = filename, file = file)
+  csv_path <-paste0("./3_staged_data/", filename, ".csv")
+  rda_path <- paste0("./3_staged_data/", filename, ".rda")
   
-  ifelse(file.exists(path),
-         message(paste0("Clean data has been saved to  ", file, 
-                        ".  The filepath is also returned by this function.")),
+  assign(filename, output)
+    # assign filt_dat to filename
+  write.csv(filt_dat, file = csv_path, row.names = FALSE)
+  save(list = filename, file = rda_path)
+  
+  ifelse(file.exists(csv_path) & file.exists(rda_path),
+         message(paste0("Clean data has been saved to ./3_staged_data/ as .rda and .csv. The .rda filepath is also returned by this function.")),
          warning(paste0("File not saved to the expected filepath."))
   )
   
-  return(file)
+  return(rda_path)
 }
